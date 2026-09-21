e619b027ff9fe43d83badb9d80f4996001822de6
juldigi <irwandj0107@gmail.com>
2026-09-21 19:13:28 +0700

    V111: align cache regression with LY300 release

--- tests/service-worker-cache.test.mjs
+++ tests/service-worker-cache.test.mjs
@@ -24,5 +24,5 @@ test('service-worker shell cache references only deployable frontend files',()=>
 });
 
 test('service-worker cache version advances with the centralized runtime release',()=>{
- assert.match(sw,/factory-digital-twin-v110-shark-tracking-20260921/);
+ assert.match(sw,/factory-digital-twin-v111-ly300-causality-20260921/);
 });
