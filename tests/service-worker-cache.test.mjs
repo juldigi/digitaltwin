362bdca356c84d634df21212cd83c8412662c480
juldigi <irwandj0107@gmail.com>
2026-09-21 20:05:45 +0700

    V115: align offline cache with Offset10 rotor-role correction

--- tests/service-worker-cache.test.mjs
+++ tests/service-worker-cache.test.mjs
@@ -24,5 +24,5 @@ test('service-worker shell cache references only deployable frontend files',()=>
 });
 
 test('service-worker cache version advances with the centralized runtime release',()=>{
- assert.match(sw,/factory-digital-twin-v114-offset5-rotors-20260921/);
+ assert.match(sw,/factory-digital-twin-v115-offset10-rotors-20260921/);
 });
