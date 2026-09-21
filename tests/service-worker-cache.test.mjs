51522677a9b6fb13b9e8836b883d749acf1a3c67
juldigi <irwandj0107@gmail.com>
2026-09-21 19:07:35 +0700

    V110: align cache regression with SHARK inspection release

--- tests/service-worker-cache.test.mjs
+++ tests/service-worker-cache.test.mjs
@@ -24,5 +24,5 @@ test('service-worker shell cache references only deployable frontend files',()=>
 });
 
 test('service-worker cache version advances with the centralized runtime release',()=>{
- assert.match(sw,/factory-digital-twin-v109-diana-tracking-20260921/);
+ assert.match(sw,/factory-digital-twin-v110-shark-tracking-20260921/);
 });
