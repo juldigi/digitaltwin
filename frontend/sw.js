650fa3fe37fe0a13eaf4b805e696d20c2b7c105e
juldigi <irwandj0107@gmail.com>
2026-09-21 20:05:30 +0700

    V115: align offline cache with Offset10 rotor-role correction

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const VERSION='factory-digital-twin-v114-offset5-rotors-20260921';
+const VERSION='factory-digital-twin-v115-offset10-rotors-20260921';
 const SHELL=['./','./index.html','./config.json','./assets/favicon.svg',
   './assets/splash-industrial-v79.webp',
   './app-shell-v79.css',
