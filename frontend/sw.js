b0985a2e671fa5680a640c2d3fef68857ef7b8b4
juldigi <irwandj0107@gmail.com>
2026-09-21 18:54:00 +0700

    V108: refresh offline cache for MEDIA100 continuous mechanics

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const VERSION='factory-digital-twin-v107-mk920-hotfoil-20260921';
+const VERSION='factory-digital-twin-v108-media100-continuous-20260921';
 const SHELL=['./','./index.html','./config.json','./assets/favicon.svg',
   './assets/splash-industrial-v79.webp',
   './app-shell-v79.css',
