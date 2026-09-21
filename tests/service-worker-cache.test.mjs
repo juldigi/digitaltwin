59ce6ad5634a554b3860515e9027ac6bd65c642d
juldigi <irwandj0107@gmail.com>
2026-09-21 18:54:16 +0700

    V108: align cache regression with MEDIA100 continuous release

--- tests/service-worker-cache.test.mjs
+++ tests/service-worker-cache.test.mjs
@@ -24,5 +24,5 @@ test('service-worker shell cache references only deployable frontend files',()=>
 });
 
 test('service-worker cache version advances with the centralized runtime release',()=>{
- assert.match(sw,/factory-digital-twin-v107-mk920-hotfoil-20260921/);
+ assert.match(sw,/factory-digital-twin-v108-media100-continuous-20260921/);
 });
