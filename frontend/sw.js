02ee61c24bdc35e8daba2a7d373eb1bd39fbc7bc
juldigi <irwandj0107@gmail.com>
2026-09-22 22:45:40 +0700

    V152: advance service worker release cache

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const VERSION='factory-digital-twin-v149-safe-shell-20260922';
+const VERSION='factory-digital-twin-v152-master-prompt-20260922';
 const SHELL=['./','./index.html','./config.json','./assets/favicon.svg',
   './assets/splash-industrial-v79.webp',
   './app-shell-v79.css',
