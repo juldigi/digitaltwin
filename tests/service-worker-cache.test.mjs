ff7ab504aa4352cdf29aedf84f53c9114c978fcc
juldigi <irwandj0107@gmail.com>
2026-09-22 23:11:37 +0700

    V153: align release-version regression expectations

--- tests/service-worker-cache.test.mjs
+++ tests/service-worker-cache.test.mjs
@@ -54,7 +54,7 @@ test('service-worker shell cache references only deployable frontend files',()=>
 });
 
 test('service-worker cache version advances with the centralized runtime release',()=>{
- assert.match(sw,/factory-digital-twin-v152-master-prompt-20260922/);
+ assert.match(sw,/factory-digital-twin-v153-foundation-scope-20260922/);
 });
 
 
